df8f0319887cf303b80b7fe54ec6d2ddd41f16cd
ObserverOfTime <chronobserver@disroot.org>
2018-05-03 19:31:21 +0300

    Update and fix stuff

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,22 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const sasslint = require('gulp-sass-lint');
+const htmllint = require('gulp-html5-lint');
 const bs = require('browser-sync').create();
 
-gulp.task('serve', ['sass'], function() {
+const files = {
+    styles: {
+        src: 'styles/scss/*.scss',
+        dist: 'styles/css/'
+    },
+    pages: [
+        './index.html',
+        'SciADVLists/*.html'
+    ]
+};
+
+function serve(done) {
     bs.init({
         notify: false,
         host: "localhost",
@@ -16,21 +29,43 @@ gulp.task('serve', ['sass'], function() {
             }
         }
     });
-    gulp.watch("./styles/scss/*.scss", ['sass']);
-    gulp.watch("**/*.html").on('change', bs.reload);
-});
+    done();
+}
 
-gulp.task('sass', function() {
-    return gulp.src("./styles/scss/*.scss")
+function reload(done) {
+    bs.reload();
+    done();
+}
+
+function compile() {
+    return gulp.src(files.styles.src)
         .pipe(sass({
             outputStyle: 'expanded',
             indentType: 'space',
             indentWidth: 4,
             linefeed: 'lf'
         }).on('error', sass.logError))
-        .pipe(gulp.dest("./styles/css"))
+        .pipe(gulp.dest(files.styles.dist))
         .pipe(bs.stream());
-});
+}
 
-gulp.task('default', ['serve']);
+function watch() {
+    gulp.watch(files.styles.src, gulp.series(lintScss, compile));
+    gulp.watch(files.pages, gulp.series(lintHtml, reload));
+}
+
+function lintScss() {
+    return gulp.src(files.styles.src)
+        .pipe(sasslint())
+        .pipe(sasslint.failOnError());
+}
+
+function lintHtml() {
+    return gulp.src(files.pages)
+        .pipe(htmllint())
+        .pipe(htmllint.failOnError());
+}
+
+gulp.task('lint', gulp.parallel(lintHtml, lintScss));
+gulp.task('default', gulp.series(serve, compile, watch));
 
